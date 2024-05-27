a9e204f9975b4fd726be149531cbd2e364649121
TonyG666 <116205783+TonyG666@users.noreply.github.com>
2024-05-27 15:22:01 -0700

    Add files via upload

---
+++ Resources.svelte
@@ -0,0 +1,51 @@
+<section id="resources">
+  <h3 class="body-header">References + Open Source</h3>
+  <p class="body-text">
+    This article is a product of the following resources:
+    <br /><br />
+  </p>
+  <div class="resource-list">
+    <p class="resource-item">
+      <a class="on-end" href="https://github.com/jwilber/svelte-interactive-article-starter">Svelte Template</a>
+      <span>(Jared Wilber)</span>
+    </p>
+    <p class="resource-item">
+      <a class="on-end" href="https://www.gold.org/">World Gold Council</a>
+    </p>
+  </div>
+</section>
+
+<style>
+  #resources {
+    padding: 2rem 1rem;
+    background-color: var(--primary);
+    color: white;
+  }
+
+  .resource-list {
+    max-width: 600px;
+    margin: 0 auto;
+    text-align: left;
+  }
+
+  .resource-item {
+    margin-bottom: 1rem;
+    font-size: 1.125rem;
+    line-height: 1.5em;
+  }
+
+  a.on-end {
+    text-decoration: underline;
+    color: #f1f3f3;
+  }
+
+  @media screen and (max-width: 950px) {
+    .resource-list {
+      max-width: 80%;
+    }
+
+    .resource-item {
+      font-size: 1rem;
+    }
+  }
+</style>
