b1793bdf2da01704467542860dd1f13b915a11d5
Yuxuan <yuxuan_zhang13@outlook.com>
2024-05-26 21:11:22 -0700

    modify svelte.config.js, rollup.config.js deploy.yaml

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
-// svelte.config.js
-const preprocess = require('svelte-preprocess');
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-module.exports = {
-  preprocess: preprocess()
+const dev = process.argv.includes('dev');
+const base_path = process.env.BASE_PATH || '';
+
+const config = {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'  // Optional: You can specify a fallback for SPA routing
+    }),
+    paths: {
+      base: dev ? '' : base_path
+    }
+  }
 };
+
+export default config;
