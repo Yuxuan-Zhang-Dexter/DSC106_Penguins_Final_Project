a812cee60a1f1a25ab897d0145cd244e11c96819
Yuxuan <yuxuan_zhang13@outlook.com>
2024-06-06 21:44:05 -0700

    add conclusion text

--- src/Components/Conclusion.svelte
+++ src/Components/Conclusion.svelte
@@ -1,5 +1,7 @@
 <h2 class="body-header">Conclusion</h2>
 <p class="body-text">
-  ...
+  Overall, our analysis shows a slight increase in gold mine production in major countries from 2010 to 2022, with no significant fluctuations. This suggests that gold mine output is not the main factor in determining gold prices. Additionally, our comprehensive dataset covers more than 100 physically-backed gold ETFs worldwide, providing regional and fund-specific analysis for the period from April 2003 to April 2025. This analysis reveals a correlation between gold ETFs and gold prices, with a significant negative correlation occurring around 2023.
+
+In summary, while gold mine production plays a role, it is not the main driver of changes in gold prices. Instead, economic uncertainty, inflation fears, and investment demand, particularly through gold ETFs, have had a greater impact on the recent surge in gold prices. Understanding these factors allows us to make more informed predictions about the future direction of gold prices.
   <br><br>
 </p>
