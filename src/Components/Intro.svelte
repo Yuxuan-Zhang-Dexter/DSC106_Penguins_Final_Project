aa849ec2972ea85b1afe3814c28f4ede21265332
TonyG666 <116205783+TonyG666@users.noreply.github.com>
2024-06-06 18:23:19 -0700

    Update Intro.svelte

--- src/Components/Intro.svelte
+++ src/Components/Intro.svelte
@@ -5,7 +5,7 @@
   <p class="body-text">
     In recent years, the price of gold has risen dramatically, sparking widespread debate about the underlying reasons. Various theories have emerged, with some attributing the increase to fluctuations in gold production, while others point to the growing demand for jewelry and other uses. In this article, we will explore the factors driving the surge in gold prices, including the dynamics of global gold mining production, the rising demand for jewelry, and other key economic influences.
   <br><br>
-  <p class="body-header">
+  <p id="reading" class="body-header">
     Want to dig up some gold before viewing the project? Click <a href="#gameButton">here</a>.
   </p>
 </section>
